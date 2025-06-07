eef8d5d856ccc1562542460f5792da229af1652f
AngelF4 <angelfhernandez71@gmail.com>
2025-06-07 12:46:39 -0600

    Initial Commit

---
+++ RetoSalud/RetoSaludApp.swift
@@ -0,0 +1,17 @@
+//
+//  RetoSaludApp.swift
+//  RetoSalud
+//
+//  Created by Angel HG on 07/06/25.
+//
+
+import SwiftUI
+
+@main
+struct RetoSaludApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
